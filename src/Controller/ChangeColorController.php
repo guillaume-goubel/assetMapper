452f61116cb1ee4d6b93b56ebbade86fb4866b60
guillaume <guillaume.goubel.pro@gmail.com>
2024-01-12 15:32:33 +0100

    change color pages

---
+++ src/Controller/ChangeColorController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ChangeColorController extends AbstractController
+{
+    #[Route('/change-color-hover-css', name: 'change_color_hover_css')]
+    public function changeColor(): Response
+    {
+        return $this->render('pages/change-color-hover-css.html.twig');
+    }
+
+    #[Route('/change-color-hover-js', name: 'change_color_hover_js')]
+    public function changeColorHoverJs():Response
+    {
+        return $this->render('pages/change-color-hover-js.html.twig');
+    }
+}
+
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DemoController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/demo', name: 'app_demo')]
+    #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return $this->render('demo/index.html.twig', [
-            'controller_name' => 'DemoController',
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
